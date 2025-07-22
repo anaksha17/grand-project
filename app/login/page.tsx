d2f608d1cfdbc404ed26395a05f6bb464ed3a7c8
anaksha17 <k224293@nu.edu.pk>
2025-07-23 01:13:01 +0500

    Basic setup

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <input type="email" placeholder="Enter your email" />
+        <button type="submit">Send Magic Link</button>
+      </form>
+    </div>
+  );
+}
